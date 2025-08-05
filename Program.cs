5a78b3daeffbf33459ff4efeb23a0e433497569a
Luiz Pereira Lucena Neto <luizpereiraln@hotmail.com>
2025-08-05 17:15:05 -0300

    Implementa classes Iphone e Nokia com testes
    Adicionadas as classes `Iphone` e `Nokia`, que herdam de `Smartphone` e implementam o método `InstalarAplicativo`. Atualizadas as propriedades da classe `Smartphone` para incluir `Modelo`, `Imei` e `Memoria`, com modificações no construtor. O arquivo `Program.cs` agora contém testes para as novas classes. Incluído o arquivo de solução `DesafioPOO.sln` para configuração no Visual Studio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Teste com Iphone
+Console.WriteLine("Teste com iPhone:");
+Smartphone iphone = new Iphone(numero: "11999999999", modelo: "iPhone 13", imei: "IMEI12345", memoria: 128);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+// Teste com Nokia
+Console.WriteLine("Teste com Nokia:");
+Smartphone nokia = new Nokia(numero: "21988888888", modelo: "Nokia 3310", imei: "IMEI67890", memoria: 32);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
